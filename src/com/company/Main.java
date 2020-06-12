749f364186f827a2124aaedff7d78f99d3d9deaf
Kaydon Stubbs <kaydons@oxbowacademy.net>
2020-06-11 20:05:33 -0600

    checks if user input is the same as the phone location

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        //***************************************Board Demensions*******************************************//
          int columns = 10 ;
          int rows = 10;
+         UserChoice userChoice = new UserChoice();
 
+
+
+         //**************************************Game board Set up*******************************************//
         Cell[][] board = new Cell[rows][columns];
 
         // build game board
@@ -26,26 +32,47 @@ public class Main {
             System.out.println();
         }
 
+
+        //************************************************Set phone locaton********************************//
         Cell phoneLocation = SetPhoneLocation(columns, rows);
+        board[(int) phoneLocation.getX()][(int) phoneLocation.getY()].setPhone(true);
+        System.out.println(board[(int) phoneLocation.getX()][(int) phoneLocation.getY()].isPhone());
+        board[(int) phoneLocation.getX()][(int) phoneLocation.getY()].setSymbol('*');
+
+        for( int i = 0 ; i <rows; i++){
+            for( int j = 0; j <columns; j++){
+                System.out.print(board[i][j].getSymbol() +"  ");
+            }
+            System.out.println();
+        }
         System.out.println(phoneLocation.getX() + " " + phoneLocation.getY());
-        boolean pass;
-        pass = testPhoneLocationInMap(columns, rows);
-        if( pass ==true)
-            System.out.println("passed");
-        else
-            System.out.println("failed");
+        userChoice = setUserChoice();
+        System.out.println(userChoice.getX() +" "+ userChoice.getY());
+        System.out.println(IsChoicePhoneLocation(userChoice, phoneLocation));
+
+        //****************************************Tests****************************************************//
+       // boolean pass;
+       // pass = testPhoneLocationInMap(columns, rows);
+      //  if( pass ==true)
+       //     System.out.println("passed");
+       // else
+      //      System.out.println("failed");
         //System.out.println(getDistance(board[0][0],board[1][1]));
         //System.out.println("Hello world");
         //setUserChoice();
 
     }
 
+
+    //***************************************Distance Formula*************************************************//
     public static double getDistance ( Cell phone, Cell Clicked){
         double distance = 0;
         distance = Math.sqrt(((Clicked.getX()-phone.getX())*(Clicked.getX()-phone.getX()))+((Clicked.getY()-phone.getY())*(Clicked.getY()-phone.getY())));
         return distance;
     }
 
+
+    //************************************************Get User Choice******************************************//
     public static UserChoice setUserChoice(){
         UserChoice userChoice = new UserChoice();
         double x= 0.0;
@@ -62,6 +89,8 @@ public class Main {
         return userChoice;
     }
 
+
+    //*************************************Get Randome phone starting location******************************//
     public static Cell SetPhoneLocation(int col, int row){
         Cell cell = new Cell();
         cell.setY( (int) (Math.random() * col - 1));
@@ -69,6 +98,19 @@ public class Main {
         return cell;
     }
 
+
+    //*************************************Check if user choice is phone location******************************//
+    public static boolean IsChoicePhoneLocation (UserChoice userChoice, Cell phone){
+        boolean b = false;
+        if (userChoice.getX()==phone.getX() && userChoice.getY()== phone.getY()){
+            b = true;
+        }
+            return b;
+    }
+
+    //**************************************Set Timer********************************************************//2
+
+    //*****************************************Test Functions*************************************************//
     public static boolean testPhoneLocationInMap(int col, int rows){
         Cell cell = new Cell();
         for (int x=0; x<1000000; x++){
@@ -81,6 +123,12 @@ public class Main {
             }
         }
         return true;
-    }x
+    }
+
+    public static boolean tetsPhonelocationisUserchoice(UserChoice userChoice, Cell phone){
+        boolean pass = false;
+
+        return true;
+    }
 
 }
